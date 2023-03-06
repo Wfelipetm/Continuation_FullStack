900dc3a8ff479db5b57219c88d0f2ea8b35f26a5
Wallace Tavares <wfelipetm@gmail.com>
2023-03-06 16:50:09 -0300

    Create Editora.ts

---
+++ 2ºsemestre/N3/livros-react/src/modelo/Editora.ts
@@ -0,0 +1,13 @@
+// export default class Editora {
+//     public codEditora: number;
+//     public nome: string;
+//
+//     constructor(codEditora: number, nome: string) {
+//         this.codEditora = codEditora;
+//         this.nome = nome;
+//     }
+// }
+export default interface Editora {
+    codEditora: number;
+    nome: string;
+}
