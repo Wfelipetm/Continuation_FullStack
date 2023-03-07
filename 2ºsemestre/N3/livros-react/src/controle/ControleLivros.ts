3a3f8f801eb2626818ebe003fa3108f71bb36d5d
Wallace Tavares <wfelipetm@gmail.com>
2023-03-07 00:10:05 -0300

    Update ControleLivros.ts

--- 2ºsemestre/N3/livros-react/src/controle/ControleLivros.ts
+++ 2ºsemestre/N3/livros-react/src/controle/ControleLivros.ts
@@ -3,21 +3,21 @@ import Livro from "../modelo/Livro";
 const livros : Array<Livro> = [{
     codigo: 1,
     codEditora: 1,
-    titulo: "Título 1",
-    resumo: "Resumo 1",
-    autores: ["Autor 1", "Autor 2", "Autor 3"]
+    titulo: "Use a Cabeça: Java",
+    resumo: "Use a Cabeça! Java é uma experiência completa de aprendizado em programação orientada a objectos (OO) e Java.",
+    autores: ["Bert Bates", "Kathy Sierra"]
 },{
     codigo: 2,
     codEditora: 2,
-    titulo: "Título 2",
-    resumo: "Resumo 2",
-    autores: ["Autor 1", "Autor 2", "Autor 3"]
+    titulo: "Java como Progamar",
+    resumo: "Milhões de alunos e profissionais aprenderam programação e desenvolvimento de software com os livros Deitel",
+    autores: ["Paul Deitel", "HarveyDeitel"]
 },{
     codigo: 3,
     codEditora: 3,
-    titulo: "Título 3",
-    resumo: "Resumo 3",
-    autores: ["Autor 1", "Autor 2", "Autor 3"]
+    titulo: "Core Java for the Impatient",
+    resumo: "eaders familiar with Horstmanns original, two-volume Core Java” books Addison Wesley who are looking for a comprehensive, but condensed guide to all of the new features and functions of Java SE 9 will learn how these new features impact the language and core libraries.",
+    autores: ["CayHorstmann", "WallaceTavares"]
 }];
 
 export default class ControleLivro {
