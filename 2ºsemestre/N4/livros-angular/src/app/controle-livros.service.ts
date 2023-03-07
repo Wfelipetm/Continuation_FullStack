bab69f19c2855fe990f9ff721970c89df215c6c7
Wallace Tavares <wfelipetm@gmail.com>
2023-03-06 23:55:31 -0300

    Update controle-livros.service.ts

--- 2ºsemestre/N4/livros-angular/src/app/controle-livros.service.ts
+++ 2ºsemestre/N4/livros-angular/src/app/controle-livros.service.ts
@@ -18,14 +18,14 @@ export class ControleLivrosService {
       codEditora: 2,
       titulo: 'Java, como Progamar',
       resumo: 'Milhões de alunos e profissionais aprenderam programação e desenvolvimento de software com os livros Deitel',
-      autores: ['Paul Deitel', 'Harvey Deitel']
+      autores: ['Paul Deitel','HarveyDeitel']
     },
     {
       codigo: 3,
       codEditora: 3,
-      titulo: 'Livro 3',
-      resumo: 'Resumo do Livro 3',
-      autores: ['Autor 5']
+      titulo: 'Core Java for the Impatient',
+      resumo: 'eaders familiar with Horstmanns original, two-volume "Core Java” books Addison Wesley who are looking for a comprehensive, but condensed guide to all of the new features and functions of Java SE 9 will learn how these new features impact the language and core libraries.',
+      autores: ['CayHorstmann']
     }
   ];
 
