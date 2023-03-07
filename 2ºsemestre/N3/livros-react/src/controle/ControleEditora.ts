380820b44ea44f08efbab9bde92a5f384a5b969b
Wallace Tavares <wfelipetm@gmail.com>
2023-03-07 00:10:02 -0300

    Update ControleEditora.ts

--- 2ºsemestre/N3/livros-react/src/controle/ControleEditora.ts
+++ 2ºsemestre/N3/livros-react/src/controle/ControleEditora.ts
@@ -2,13 +2,13 @@ import Editora from "../modelo/Editora";
 
 const editoras : Array<Editora> = [{
     codEditora: 1,
-    nome: "Editora 1"
+    nome: "Alta Books"
 },{
     codEditora: 2,
-    nome: "Editora 2"
+    nome: "Pearson"
 },{
     codEditora: 3,
-    nome: "Editora 3"
+    nome: "Addison Wesley"
 }];
 
 export default class ControleEditora {
