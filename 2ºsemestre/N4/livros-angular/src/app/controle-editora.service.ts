1a6e070912f5d798ee05b0dff12d81372c401c82
Wallace Tavares <wfelipetm@gmail.com>
2023-03-08 01:45:02 -0300

    Update controle-editora.service.ts

--- 2ºsemestre/N4/livros-angular/src/app/controle-editora.service.ts
+++ 2ºsemestre/N4/livros-angular/src/app/controle-editora.service.ts
@@ -5,12 +5,17 @@ import { Editora } from './editora';
   providedIn: 'root'
 })
 export class ControleEditoraService {
-  editoras: Array<Editora> = [
-    { codEditora: 1, nome: 'Alta Books' },
-    { codEditora: 2, nome: 'Pearson' },
-    { codEditora: 3, nome: 'Addison Wesley' }
-  ];
-  constructor() { }
+  editoras: Array<Editora> = [{
+      codEditora: 1, 
+      nome: 'Alta Books' 
+  },{ 
+      codEditora: 2, 
+      nome: 'Pearson' 
+  },{
+      codEditora: 3, 
+      nome: 'Addison Wesley' ,
+}];
+ 
 
   getEditoras(): Array<Editora> {
     return this.editoras;
